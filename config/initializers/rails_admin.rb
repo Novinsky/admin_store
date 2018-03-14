576c4fbdbd9001523413bec89f00fa8ad09b69b4
Renan Bona <rcbp3094@gmail.com>
2018-03-14 00:31:59 -0300

    Rails admin and some models translated

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,5 +1,13 @@
 RailsAdmin.config do |config|
 
+  config.main_app_name = ["ArtDec", ""]
+
+  config.navigation_static_links = {
+  'ArtDec' => 'https://artdec.com'
+  }
+
+  config.navigation_static_label = "Lins Úteis"
+
   ### Popular gems integration
 
   ## == Devise ==
@@ -116,6 +124,32 @@ RailsAdmin.config do |config|
       field  :address
 
     end
+  end
+
+  config.model Discount do
+    parent Product
+  end
+
+  config.model Sale do
+    parent User
+    weight -2
+  end
+
+  config.model Comission do
+    parent User
+    weight -1
+  end
+
+  config.model Client do
+    parent User
+  end
+
+  config.model ProductQuantity do
+    visible false
+  end
+
+  config.model Address do
+    visible false
   end
 
   config.model ProductQuantity do
